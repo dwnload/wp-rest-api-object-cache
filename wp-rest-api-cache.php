3ecedb1a9abecd4af727092414293f76724d6bc3
Austin Passy <austin@passy.co>
2018-02-02 13:59:48 -0800

    Remove uninstall hook, and register ignore update notice for plugin.

--- wp-rest-api-cache.php
+++ wp-rest-api-cache.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: WP REST API Cache
+ * Plugin Name: REST API Object Cache
  * Description: Enable object caching for WordPress' REST API. Aids in increased response times of your applications endpoints.
  * Author: Austin Passy
  * Author URI: http://github.com/thefrosty
@@ -8,7 +8,7 @@
  * Requires at least: 4.9
  * Tested up to: 4.9
  * Requires PHP: 7.0
- * Plugin URI: https://github.com/thefrosty/wp-rest-api-cache
+ * Plugin URI: https://github.com/thefrosty/wp-rest-api-object-cache
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -17,9 +17,18 @@ use TheFrosty\WP\Utils\Init;
 use Dwnload\WpRestApi\WpRestApiCache;
 
 add_action( 'init', function() {
-    ( new WpRestApiCache( new Init() ) )->initiateWpHooksInterfaces()->getInit()->initialize();
+    ( new WpRestApiCache( new Init() ) )->initiateWpHooks()->getInit()->initialize();
 } );
 
-register_uninstall_hook( __FILE__, static function() {
-    call_user_func( [ ( new WpRestApiCache() )->getRestDispatch(), 'wpCacheFlush' ] );
-} );
+call_user_func_array(
+    function( $filter ) {
+        add_filter( $filter, function( $value ) use ( $filter ) {
+            if ( isset( $value ) && is_object( $value ) ) {
+                unset( $value->response[ plugin_basename( __FILE__ ) ] );
+            }
+
+            return $value;
+        } );
+    },
+    [ 'pre_site_transient_update_plugins', 'site_transient_update_plugins' ]
+);
