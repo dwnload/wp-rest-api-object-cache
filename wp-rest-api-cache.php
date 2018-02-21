397ab3c61271f5b5dc362fa77e94c39afd189bdc
Austin Passy <austin@passy.co>
2018-02-21 13:40:45 -0800

    Check for non empty response in value and that the current plugin has a key in the response array.

--- wp-rest-api-cache.php
+++ wp-rest-api-cache.php
@@ -4,7 +4,7 @@
  * Description: Enable object caching for WordPress' REST API. Aids in increased response times of your applications endpoints.
  * Author: Austin Passy
  * Author URI: http://github.com/thefrosty
- * Version: 0.2
+ * Version: 0.2.1
  * Requires at least: 4.9
  * Tested up to: 4.9
  * Requires PHP: 7.0
@@ -23,7 +23,7 @@ add_action( 'init', function() {
 call_user_func_array(
     function( $filter ) {
         add_filter( $filter, function( $value ) use ( $filter ) {
-            if ( isset( $value ) && is_object( $value ) ) {
+            if ( ! empty( $value->response ) && array_key_exists( plugin_basename( __FILE__ ), $value->response ) ) {
                 unset( $value->response[ plugin_basename( __FILE__ ) ] );
             }
 
